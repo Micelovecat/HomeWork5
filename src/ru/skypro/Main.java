34a9537740018d9e76282bcc36f8245550711f1b
Micelovecat <flora_bux@mail.ru>
2021-12-01 02:08:06 +0300

    Second commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,9 +1,62 @@
 package ru.skypro;
 
 public class Main {
+//     HomeWork5Easy1();
+//     HomeWork5Easy2();
+//     HomeWork5Easy3();
 
-    public static void main(String[] args) {
-        System.out.println("");
+    public static void HomeWork5Easy1() {
+        System.out.println("HomeWork5. Exercise 1.");
+        int[] integers = new int[3];
+        integers[0] = 1;
+        integers[1] = 2;
+        integers[2] = 3;
+        int firstInteger = integers[0];
+        int secondInteger = integers[1];
+        int thirdInteger = integers[2];
+        System.out.println(firstInteger);
+        System.out.println(secondInteger);
+        System.out.println(thirdInteger);
 
+        float[] fractionalNumbers = {1.57f, 7.654f, 9.986f};
+        fractionalNumbers[0] = 1.57f;
+        float firstFractionalNumb = fractionalNumbers[0];
+        float secondFractionalNumb = fractionalNumbers[1];
+        float thirdFractionalNumb = fractionalNumbers[2];
+        System.out.println(firstFractionalNumb);
+        System.out.println(secondFractionalNumb);
+        System.out.println(thirdFractionalNumb);
+
+        float[] numbers = new float[6];
+        numbers[0] = 0.5f;
+        numbers[1] = 1f;
+        numbers[2] = 1.5f;
+        numbers[3] = 2f;
+        numbers[4] = 2.5f;
+        numbers[5] = 3f;
+        float firstNumber = numbers[0];
+        System.out.println(firstNumber);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
+
+    public static void HomeWork5Easy2() {
+        System.out.println("HomeWork5. Exercise 2.");
+
+        }
+
+//    public static void main(String[] args) {
+//        int[] weights = {90,  91, 93, 92,85, 87, 89, 0, 0, 0, 0};
+//        weights[0] = 90;
+//        int januaryWeight = weights[0];
+//        System.out.println(januaryWeight);
+//        System.out.println(weights[0]);
+//        System.out.println(weights[4]);
+//        int january = 0;
+//       System.out.println(weights[january]);
+//        for (int i = 0; i < weights.length; i++) {
+//            System.out.println(weights[i]);
+//        }
+//    }
 }
