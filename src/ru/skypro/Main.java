2ddef340fe8adb4955711f867b14580df98bac0b
Micelovecat <flora_bux@mail.ru>
2021-12-04 23:55:55 +0300

    Third commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,62 +1,78 @@
 package ru.skypro;
 
+import java.util.Arrays;
+
 public class Main {
-//     HomeWork5Easy1();
-//     HomeWork5Easy2();
-//     HomeWork5Easy3();
+    public static void main(String[] args) {
 
-    public static void HomeWork5Easy1() {
-        System.out.println("HomeWork5. Exercise 1.");
-        int[] integers = new int[3];
-        integers[0] = 1;
-        integers[1] = 2;
-        integers[2] = 3;
-        int firstInteger = integers[0];
-        int secondInteger = integers[1];
-        int thirdInteger = integers[2];
-        System.out.println(firstInteger);
-        System.out.println(secondInteger);
-        System.out.println(thirdInteger);
+//        HomeWork5Easy1();
+            System.out.println("HomeWork5. Exercise 1.");
+            int[] integers = new int[]{1, 2, 3};
+            double[] doubles = {1.57, 7.654, 9.986};
+            float[] fractionalNumbers = new float[]{0.5f, 1.0f, 1.5f};
 
-        float[] fractionalNumbers = {1.57f, 7.654f, 9.986f};
-        fractionalNumbers[0] = 1.57f;
-        float firstFractionalNumb = fractionalNumbers[0];
-        float secondFractionalNumb = fractionalNumbers[1];
-        float thirdFractionalNumb = fractionalNumbers[2];
-        System.out.println(firstFractionalNumb);
-        System.out.println(secondFractionalNumb);
-        System.out.println(thirdFractionalNumb);
+//        HomeWork5Easy2();
+            System.out.println("HomeWork5. Exercise 2.");
+            for (int i = 0; i < integers.length; i++) {
+                if (i == integers.length - 1) {
+                    System.out.println(integers[i]);
+                } else {
+                    System.out.print(integers[i] + ", ");
+                }
+            }
+            for (int i = 0; i < doubles.length; i++) {
+                if (i == doubles.length - 1) {
+                    System.out.println(doubles[i]);
+                } else {
+                    System.out.print(doubles[i] + ", ");
+                }
+            }
+            for (int i = 0; i < fractionalNumbers.length; i++) {
+                if (i == fractionalNumbers.length - 1) {
+                    System.out.println(fractionalNumbers[i]);
+                } else {
+                    System.out.print(fractionalNumbers[i] + ", ");
+                }
+            }
+            System.out.println();
 
-        float[] numbers = new float[6];
-        numbers[0] = 0.5f;
-        numbers[1] = 1f;
-        numbers[2] = 1.5f;
-        numbers[3] = 2f;
-        numbers[4] = 2.5f;
-        numbers[5] = 3f;
-        float firstNumber = numbers[0];
-        System.out.println(firstNumber);
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+//       HomeWork5Easy3();
+            System.out.println("HomeWork5. Exercise 3.");
+            for (int i = integers.length - 1; i >= 0; i--) {
+                if (i == 0) {
+                    System.out.println(integers[i]);
+                } else {
+                    System.out.print(integers[i] + ", ");
+                }
+            }
+
+            for (int i = doubles.length - 1; i >= 0; i--) {
+                if (i == 0) {
+                    System.out.println(doubles[i]);
+                } else {
+                    System.out.print(doubles[i] + ", ");
+                }
+            }
+
+            for (int i = fractionalNumbers.length - 1; i >= 0; i--) {
+                if (i == 0) {
+                    System.out.println(fractionalNumbers[i]);
+                } else {
+                    System.out.print(fractionalNumbers[i] + ", ");
+                }
+            }
+            System.out.println();
+
+//       HomeWork5Easy4();
+            System.out.println("HomeWork5. Exercise 4.");
+        for (int i = 0; i < integers.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(integers[i] + 1);
+            } else {
+                System.out.println(integers[i]);
+            }
         }
+        System.out.println();
     }
-
-    public static void HomeWork5Easy2() {
-        System.out.println("HomeWork5. Exercise 2.");
-
-        }
-
-//    public static void main(String[] args) {
-//        int[] weights = {90,  91, 93, 92,85, 87, 89, 0, 0, 0, 0};
-//        weights[0] = 90;
-//        int januaryWeight = weights[0];
-//        System.out.println(januaryWeight);
-//        System.out.println(weights[0]);
-//        System.out.println(weights[4]);
-//        int january = 0;
-//       System.out.println(weights[january]);
-//        for (int i = 0; i < weights.length; i++) {
-//            System.out.println(weights[i]);
-//        }
-//    }
 }
+//     HomeWork5Easy();
